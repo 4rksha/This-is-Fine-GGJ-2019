4e72272c9eb60d9ffaf923f5009054585c78f67d
Benoît Remy <benoit.remy@drivequant.com>
2019-01-26 05:18:55 +0100

    mouvement qui deconne

---
+++ Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentsInChildren<Animator>()[0];
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(rb.velocity.x > 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float vertical;
+    public float movementSpeed = 50f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        vertical = (Input.GetAxis ("Horizontal") * Time.deltaTime * movementSpeed);
+        //transform.Translate(-vertical, 0, 0);
+        rb.MovePosition(rb.position + new Vector2(vertical, 0) * Time.fixedDeltaTime);
+    }
+}
