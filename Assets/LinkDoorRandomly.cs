7b9606411cca95ea125a706cbecaaebaa3674922
nhysi <sylvain@nyssens.be>
2019-01-26 22:39:37 +0100

    Door linked randomly

---
+++ Assets/LinkDoorRandomly.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LinkDoorRandomly : MonoBehaviour
+{
+    public GameObject[] Level1;
+    public GameObject[] Level2;
+    public GameObject[] Level3;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        List<GameObject> Level1List = new List<GameObject>(Level1);
+        List<GameObject> Level2List = new List<GameObject>(Level2);
+        List<GameObject> Level3List = new List<GameObject>(Level3);
+
+        Level1List = Shuffle(Level1List);
+        Level2List = Shuffle(Level2List);
+        Level3List = Shuffle(Level3List);
+
+        LinkDoors(Level1List[Level1List.Count-1], Level2List[Level2List.Count-1]);
+        Level1List.RemoveAt(Level1List.Count - 1);
+        Level2List.RemoveAt(Level2List.Count - 1);
+
+        List<GameObject> DoorsList = new List<GameObject>();
+        DoorsList.AddRange(Level1List);
+        DoorsList.AddRange(Level2List);
+        Shuffle(DoorsList);
+
+        LinkDoors(Level3List[Level3List.Count - 1], DoorsList[DoorsList.Count - 1]);
+        Level3List.RemoveAt(Level3List.Count - 1);
+        DoorsList.RemoveAt(DoorsList.Count - 1);
+
+        DoorsList.AddRange(Level3List);
+
+        while (DoorsList.Count > 1)
+        {
+            LinkDoors(DoorsList[DoorsList.Count - 1], DoorsList[DoorsList.Count - 2]);
+            DoorsList.RemoveAt(DoorsList.Count - 1);
+            DoorsList.RemoveAt(DoorsList.Count - 1);
+        }
+
+    }
+    private void LinkDoors(GameObject door1, GameObject door2)
+    {
+        door1.GetComponent<TeleportTo>().LinkedDoor = door2;
+        door2.GetComponent<TeleportTo>().LinkedDoor = door1;
+    }
+    private List<GameObject> Shuffle(List<GameObject> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            GameObject temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+        return list;
+    }
+}
---
+++ Assets/Scripts/TeleportTo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class TeleportTo : MonoBehaviour
+{
+    public GameObject LinkedDoor;
+    public Vector3 Offset = new Vector3(0,0);
+    private Player player;
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    int playerID = collision.gameObject.GetComponent<PlayerController>().playerId;
+    //    player = ReInput.players.GetPlayer(playerID);
+    //    if (player.GetButtonDown("ButtonA"))
+    //    {
+    //        collision.transform.position = LinkedDoor.transform.position + Offset;
+    //    }
+    //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("in");
+        collision.gameObject.GetComponent<PlayerController>().DoorPos(LinkedDoor.transform.position + Offset);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("out");
+        if(collision.gameObject.GetComponent<PlayerController>().GetPos() != transform.position)
+        {
+            Debug.Log("clear pos");
+            collision.gameObject.GetComponent<PlayerController>().DoorPos(new Vector3());
+        } 
+    }
+}
