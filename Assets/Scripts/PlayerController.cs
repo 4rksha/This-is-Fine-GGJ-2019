302b3be1531d01d9967aff10cbdcf81e26c375f1
Benoît Remy <benoit.remy@drivequant.com>
2019-01-26 05:23:21 +0100

    mouvement basique ok

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float vertical;
-    public float movementSpeed = 50f;
+    public float movementSpeed = 500f;
+    public float speed = 5f;
     Rigidbody2D rb;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +24,7 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        vertical = (Input.GetAxis ("Horizontal") * Time.deltaTime * movementSpeed);
-        //transform.Translate(-vertical, 0, 0);
+        vertical = (Input.GetAxis ("Horizontal") * Time.deltaTime * movementSpeed) *10;
         rb.MovePosition(rb.position + new Vector2(vertical, 0) * Time.fixedDeltaTime);
     }
 }
