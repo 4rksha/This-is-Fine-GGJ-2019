9d387a3b9e0c3bee28da6791408ce328a283124f
Benoît Remy <benoit.remy@drivequant.com>
2019-01-26 11:08:05 +0100

    sprite flip X

--- Assets/Scripts/CharacterAnim.cs
+++ Assets/Scripts/CharacterAnim.cs
@@ -6,24 +6,34 @@ public class CharacterAnim : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponentsInChildren<Animator>()[0];
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentsInChildren<SpriteRenderer>()[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetAxis("Horizontal") > 0)
+        if (Input.GetAxis("Horizontal") != 0)
         {
             animator.SetBool("isWalking", true);
         }
         else
         {
             animator.SetBool("isWalking", false);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            sprite.flipX = true;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            sprite.flipX = false;
         }
     }
 }
